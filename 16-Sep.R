583e385c170eec4ac3abcb3a9729a69f96b5b332
jeysura1 <71433807+jeysura1@users.noreply.github.com>
2020-09-17 15:26:29 +0530

    Add files via upload

---
+++ 16-Sep.R
@@ -0,0 +1,42 @@
+x <-58
+y<-20
+z<-100
+
+x+y*z
+
+sqrt (9)
+
+pi
+
+2*pi*6378
+
+(6*3)+5%%2
+
+x<-200
+
+#Create a vector
+x<- c(12,20,15,100)
+
+vtr1<-c(2,3,4,5)
+
+vtr2<-c(1,2,3,4)
+
+#Add vectors
+vtr3<-vtr1+vtr2
+
+#multiple vector with scalar
+vtr3 *5
+
+#Accessing the elements of vector
+vtr1[2]
+
+#Replace the vector value
+vtr1[2]<-0
+
+vtr1
+
+vtr_char<- c('R','Python','Java') #homegeneous
+
+vtr_char<- c('R','Python','Java',5,6,7)
+#vector can handle one kind of data type
+
