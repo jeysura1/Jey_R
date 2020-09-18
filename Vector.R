a112833c61acfa2762898f764ac966578c2a419c
jeysura1 <71433807+jeysura1@users.noreply.github.com>
2020-09-18 19:52:40 +0530

    Add files via upload

---
+++ Vector.R
@@ -0,0 +1,36 @@
+u<-4
+v<-8
+add <- c(u+v)
+subtract <- v-u
+multiple <- v*u
+devide <- u/v
+
+power <- v^u
+# vectors with multiple elements
+
+u<- c(4,5,6)
+v<- c(1,2,3)
+add <- c(u+v)
+subtract <- v-u
+multiple <- v*u
+devide <- u/v
+power <- v^u
+
+
+w <- (2*u+v)/10
+w <-(u+.5*v)^2
+.5*v
+w <- (u+(.5*v))
+w <- ((u+(.5*v)))^2
+
+# Circle Area
+Radius<-25
+Area <- pi*Radius^2
+
+
+x <- c(1:10,2:5)
+x
+x[4]
+
+y <-
+
