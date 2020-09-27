5831039ee7cbcc4666c60554f1876d1b2d406d1e
jeysura1 <71433807+jeysura1@users.noreply.github.com>
2020-09-27 13:24:41 +0530

    Add files via upload

---
+++ R-Practice3.R
@@ -0,0 +1,16 @@
+#Load datasets
+eq<-datasets::quakes
+eq
+
+#skewness  refer website then download the pkg(install) and invoke the pkg (library)
+skewness 
+
+
+#kutosis
+
+
+#var
+
+#std deviation
+
+write.csv(tst,"tst.csv")
